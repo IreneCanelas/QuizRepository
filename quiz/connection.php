ae5e37aa48e8c81f49028b04006f7d7f2361c94a
marciandreia11 <a034928@ismai.pt>
2020-04-22 22:52:55 +0100

    Update connection.php

--- quiz/connection.php
+++ quiz/connection.php
@@ -4,10 +4,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "quizwebsite";
-$email = "email";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $email);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
