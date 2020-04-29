e0b85ac1023933ee7b22eb04749e10b24d256fce
Layla Reis <layla.sreis@gmail.com>
2020-04-30 00:21:46 +0100

    Update set_category_type_session.php

--- quiz/set_category_type_session.php
+++ quiz/set_category_type_session.php
@@ -1,13 +1,14 @@
 <?php
+    session_start();
     include "connection.php";
 
     $category=$_GET['name'];
     $_SESSION['category']=$category;
-    $res=msqli_query("SELECT * from questions where category='$category'");
+    $res=mysqli_query($conn, "SELECT * from questions where category='$category'");
 
 //tempo
 //while ($row=msqli_fetch_array($res)) {
 //   $_SESSION['']
-}
+//
 
 ?>
