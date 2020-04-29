6a0981dcdee694f40f050e0de98dee45fa652481
Layla Reis <layla.sreis@gmail.com>
2020-04-30 00:24:34 +0100

    Update initialPage.php

--- quiz/initialPage.php
+++ quiz/initialPage.php
@@ -1,56 +1,6 @@
 <?php
-include "connection.php";
-?>
-
-<?php
-
-  //Join to table
-  $sql = "SELECT id, name, photo_url FROM quizzes";
-
-  $result = $conn->query($sql);
-  $quizzes = [];
-
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          $quizzes[] = $row;
-      }
-  } else {
-      echo "0 results";
-  }
-
-
+  include "connection.php";
   include "header.php";
-?>
-
-
-<!-- CONFIRMAR SE VAI O CODIGO ABAIXO OU O INCLUDE -->
-  
-<!--Quizzes 
-<div class="center">
-  <div class="offset"> 
-    <div class="row text-center">
-      <?php
-      foreach($quizzes as $quiz) { ?>
-          <div class="col-md-4">
-              <div class="details_quiz">
-                  <h3> <?php echo $quiz['name'] ?> </h3>
-                  <img src="<?php echo $quiz['photo_url'] ?>" id="details_photo">
-                  <button class="btn btn-outline-dark" type="submit" onclick='set_category_type_session'> Fazer este! </button>
-              </div>
-          </div>
-          <?php
-      }?>
-    </div>
-  </div>
-</div>-->
-
-<?php
-    include "select_exam.php";
-?>
-
-<!-- Fim Quizzes -->
-
-<?php 
+  include "select_exam.php";
   include "footer.html";
 ?>
