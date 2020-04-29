e8a40373df7025440323560a70df81dda0e56db9
Layla Reis <layla.sreis@gmail.com>
2020-04-30 00:25:00 +0100

    Update select_exam.php

--- quiz/select_exam.php
+++ quiz/select_exam.php
@@ -3,6 +3,25 @@
     include "connection.php";
 ?>
 
+<?php
+
+  //Join to table
+  $sql = "SELECT id, name, photo_url FROM quizzes";
+
+  $result = $conn->query($sql);
+  $quizzes = [];
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          $quizzes[] = $row;
+      }
+  } else {
+      echo "0 results";
+  }
+
+?>
+
 <!-- Categoria dos Quizzes -->
 <div class="center">
   <div class="offset"> 
@@ -14,7 +33,7 @@
                   <h3> <?php echo $quiz['name'] ?> </h3>
                   <img src="<?php echo $quiz['photo_url'] ?>" id="details_photo">
                   <!--onclick abrir questões-->
-                  <button class="btn btn-outline-dark" type="submit" onclick="document.location.href='load_questions.php'">Fazer este!</button>
+                  <button class="btn btn-outline-dark" type="submit" onclick="set_category_type_session(this.value);">Fazer este!</button>
               </div>
           </div>
           <?php
@@ -23,3 +42,16 @@
   </div>
 </div>
 
+
+<script type="text/javascript">
+  function set_category_type_session(category) {
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        window.location = "dashboard.php";
+      }
+    };
+    xmlhttp.open("GET","set_category_type_session.php?category="+category, true);
+    xmlhttp.send(null);
+  }
+</script>
