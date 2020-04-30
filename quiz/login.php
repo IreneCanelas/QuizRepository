3af9827dacd707ef664434675a6307579c0fb20d
marciandreia11 <a034928@ismai.pt>
2020-04-30 12:34:27 +0100

    Update login.php
    login 5

--- quiz/login.php
+++ quiz/login.php
@@ -8,7 +8,7 @@ session_start();
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['email'], $_POST['psw']) ) {
 	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+	exit('Por favor preencha os campos email e password');
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
