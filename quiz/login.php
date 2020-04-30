5fc93c412554e5b45f6f348fe3be71fa33957161
marciandreia11 <a034928@ismai.pt>
2020-04-30 12:33:02 +0100

    Update login.php
    login teste 4

--- quiz/login.php
+++ quiz/login.php
@@ -4,17 +4,6 @@ include "connection.php";
 include "registo.php";
 
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['email'], $_POST['psw']) ) {
@@ -50,7 +39,6 @@ if ($stmt = $con->prepare('SELECT id, psw FROM registos WHERE email = ?')) {
 	} else {
 		echo 'Incorrect email!';
 	}
-
 
 	$stmt->close();
 }
