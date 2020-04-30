cca3ef9b0f8aaccb949f67e8032d86d34cb6acf4
Layla Reis <layla.sreis@gmail.com>
2020-04-30 15:50:34 +0100

    Update select_exam.php
    Configuração dos botões, depois retornar imagens.

--- quiz/select_exam.php
+++ quiz/select_exam.php
@@ -1,57 +1,38 @@
 <?php
     //session_start();
+
     include "connection.php";
+    include "header.php";
 ?>
 
-<?php
+<div>
+  <?php 
+    $res=mysqli_query($conn,"select * from quizzes");
+    while($row=mysqli_fetch_array($res))
+    {
+      ?>
+      <input type="button" class="btn btn-success form-control" value="<?php echo $row["name"]; ?>" style="margin-top: 10px; background-color: blue; color: white" onclick="set_category_type_session(this.value);">
 
-  //Join to table
-  $sql = "SELECT id, name, photo_url FROM quizzes";
-
-  $result = $conn->query($sql);
-  $quizzes = [];
-
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          $quizzes[] = $row;
-      }
-  } else {
-      echo "0 results";
-  }
-
-?>
-
-<!-- Categoria dos Quizzes -->
-<div class="center">
-  <div class="offset"> 
-    <div class="row text-center">
       <?php
-      foreach($quizzes as $quiz) { ?>
-          <div class="col-md-4">
-              <div class="details_quiz">
-                  <h3> <?php echo $quiz['name'] ?> </h3>
-                  <img src="<?php echo $quiz['photo_url'] ?>" id="details_photo">
-                  <!--onclick abrir questões-->
-                  <button class="btn btn-outline-dark" type="submit" onclick="set_category_type_session(this.value);">Fazer este!</button>
-              </div>
-          </div>
-          <?php
-      }?>
-    </div>
-  </div>
+    } 
+    ?>
 </div>
 
+<?php 
+  include "footer.html";
+?>
 
+  
 <script type="text/javascript">
-  function set_category_type_session(category) {
+  function set_category_type_session(quizzes) {
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function() {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         window.location = "dashboard.php";
       }
     };
-    xmlhttp.open("GET","set_category_type_session.php?category="+category, true);
+    xmlhttp.open("GET","foarajax/set_category_type_session.php?quizzes="+ quizzes, true);
     xmlhttp.send(null);
   }
 </script>
+
