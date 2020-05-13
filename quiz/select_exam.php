b1a7200fa4232471238dff7457944c1229f29508
sararocha25 <sararaquel2621@gmail.com>
2020-05-13 22:47:28 +0100

    Update select_exam.php

--- quiz/select_exam.php
+++ quiz/select_exam.php
@@ -1,7 +1,7 @@
 <?php
-    //session_start();
-
-    include "connection.php";
+  //session_start();
+  include "connection.php";
+  //include "set_category_type_session.php";
 ?>
 
 <!-- Categoria dos Quizzes -->
@@ -15,7 +15,7 @@
                   <h3> <?php echo $quiz['name'] ?> </h3>
                   <img src="<?php echo $quiz['photo_url'] ?>" id="details_photo">
                   <!--onclick abrir questões-->
-                  <button class="btn btn-outline-dark" type="submit" onclick="set_category_type_session(this.value);">Fazer este!</button>
+                  <a href="questions_for_quiz.php?category=<?php echo $quiz['name'] ?>"> <button class="btn btn-outline-dark" type="submit" >Fazer este!</button> </a>
               </div>
           </div>
           <?php
@@ -23,17 +23,3 @@
     </div>
   </div>
 </div>
-
-  
-<script type="text/javascript">
-  function set_category_type_session(quizzes) {
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        window.location = "dashboard.php";
-      }
-    };
-    xmlhttp.open("GET","set_category_type_session.php?quizzes="+ quizzes, true);
-    xmlhttp.send(null);
-  }
-</script>
