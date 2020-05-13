caeccf3baae3cbad7e05e903f1d62dd923d397c3
marciandreia11 <a034928@ismai.pt>
2020-05-13 11:48:01 +0100

    Update header.php

--- quiz/header.php
+++ quiz/header.php
@@ -50,7 +50,7 @@ include "registo.php";
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Ínicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Os nossos quizzes</a>
