1e43b9664de47eb447178333cb7ce508f2ff0074
marciandreia11 <a034928@ismai.pt>
2020-05-13 15:54:24 +0100

    Update login.php

--- quiz/login.php
+++ quiz/login.php
@@ -3,33 +3,33 @@
 include "connection.php";
 include "registo.php";
 
-session_start();     
-    //O campo email e password preenchido entra no if para validar
+session_start(); 
+    //O campo usuário e senha preenchido entra no if para validar
     if((isset($_POST['email'])) && (isset($_POST['psw']))){
         $email = mysqli_real_escape_string($conn, $_POST['email']);
-		$psw = mysqli_real_escape_string($conn, $_POST['psw']);
-		
-		
-        //Buscar na tabela registos o user que corresponde com os dados colocados no formulário
-        $result_registo = "SELECT * FROM registos WHERE email = '$email' && psw = '$psw' LIMIT 1";
-        $resultado_registo = mysqli_query($conn, $result_registo);
-        $resultado = mysqli_fetch_assoc($resultado_usuario);
+        $psw = mysqli_real_escape_string($conn, $_POST['psw']);
+
+            
+        //Buscar na tabela registos o usuário que corresponde com os dados digitado no formulário
+        $result_registos = "SELECT * FROM registos WHERE email = '$email' && psw = '$psw' LIMIT 1";
+        $resultado_registos = mysqli_query($conn, $result_registos);
+        $resultado = mysqli_fetch_assoc($resultado_registos;
         
-        //Encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
+        //Encontrado um user na tabela usuário com os mesmos dados digitado no formulário
         if(isset($resultado)){
-            $_SESSION['registoId'] = $resultado['id'];
-            $_SESSION['registoName'] = $resultado['name'];
-            $_SESSION['registoEmail'] = $resultado['email'];
+            $_SESSION['registosId'] = $resultado['id'];
+            $_SESSION['registosName'] = $resultado['name'];
+            $_SESSION['registosEmail'] = $resultado['email'];
         //Não foi encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
         //redireciona o usuario para a página de login
         }else{    
             //Váriavel global recebendo a mensagem de erro
-            $_SESSION['loginErro'] = "Email ou password Inválido";
+            $_SESSION['loginErro'] = "Email ou password inválidos";
             header("Location: initialPage.php");
         }
     //O campo usuário e senha não preenchido entra no else e redireciona o usuário para a página de login
     }else{
-        $_SESSION['loginErro'] = "Email ou password inválido";
+        $_SESSION['loginErro'] = "Email ou password inválidos";
         header("Location: initialPage.php");
     }
 
