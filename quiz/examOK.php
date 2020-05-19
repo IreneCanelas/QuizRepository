c4093cba431e3dfbed43caa0d2a6d57196758a2c
sararocha25 <sararaquel2621@gmail.com>
2020-05-20 00:46:35 +0100

    Update examOK.php
    Correção da responsividade da página

--- quiz/examOK.php
+++ quiz/examOK.php
@@ -56,7 +56,7 @@
 
 <!--TITULO DA CATEGORIA   OK  -->
 
-<div class="card centerdash mt-4" style="width: 50rem; margin:0 auto;">
+<div class="card centerdash col-md-4" style=" margin:0 auto;">
   <div class="card-body">
       <div>
       <div class="text-center">
