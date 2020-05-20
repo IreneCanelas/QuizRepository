20087dbc2c4bab4e7de40a3329470e5890376d84
Irene Canelas <irenecanelas10@gmail.com>
2020-05-20 18:20:04 +0100

    Update header2.php

--- quiz/header2.php
+++ quiz/header2.php
@@ -39,10 +39,10 @@ include "connection.php";
 <body> 
 <!--Barra de navegação-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="initialPage.php">
+  <a class="navbar-brand" href="initialAfterLogin.php">
     <img src="images/Logo.png" width="60" height="60" alt="">
   </a>
-  <a class="navbar-brand" href="initialPage.php">QuizQuiz</a>
+  <a class="navbar-brand" href="initialAfterLogin.php">QuizQuiz</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -50,7 +50,7 @@ include "connection.php";
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="initialPage.php">Ínicio</a>
+        <a class="nav-link" href="initialAfterLogin.php">Ínicio</a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="#">Os nossos quizzes <span class="sr-only">(current)</span> </a>
