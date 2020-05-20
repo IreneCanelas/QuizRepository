b3cd5e6a4c52cec030dc8742eb87db34cef98b30
Layla Reis <layla.sreis@gmail.com>
2020-05-20 22:01:52 +0100

    Update initialAfterLogin.php

--- quiz/initialAfterLogin.php
+++ quiz/initialAfterLogin.php
@@ -1,5 +1,12 @@
 <?php
   include "connection.php";
-  include "header.php";
+  include "header2.php";
+  include "select_exam.php";
   include "footer.html";
 ?>
+
+<!--Configurar Logout-->
+
+<!--Gravar id durante a sessão-->
+
+<!--Mostrar resultados de acordo com a tabela na base dados-->
