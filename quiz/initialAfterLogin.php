d83291742e243eb61b2c6f26a5981e20c5ad8839
sararocha25 <sararaquel2621@gmail.com>
2020-05-20 22:11:14 +0100

    Update initalAfterLogin.php
    Correção de ligação entre páginas

--- quiz/initialAfterLogin.php
+++ quiz/initialAfterLogin.php
@@ -40,10 +40,10 @@
 <body> 
 <!--Barra de navegação-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="initialPage.php">
+  <a class="navbar-brand" href="initialAfterLogin.php">
     <img src="images/Logo.png" width="60" height="60" alt="">
   </a>
-  <a class="navbar-brand" href="initialPage.php">QuizQuiz</a>
+  <a class="navbar-brand" href="initialAfterLogin.php">QuizQuiz</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
