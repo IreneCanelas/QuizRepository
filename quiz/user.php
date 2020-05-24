2dceff329d80b68d2d5f7f52848faa0112af7cfc
Layla Reis <layla.sreis@gmail.com>
2020-05-24 23:45:14 +0100

    Update user.php

--- quiz/user.php
+++ quiz/user.php
@@ -1,39 +1,66 @@
 <?php
     include "headerAfterLogin.php";
     include "connection.php";
-
-
-    $sq = "SELECT id, user_id, category_id, score, num_questions FROM result";
-    $result = $conn->query($sq);
-    $user = [];
-
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $user[] = $row;
-        }
-    } //else {
-      //  echo "0 results";   
-    //}
-
+    session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> QuizQuiz Website </title>
+<div class="container padding-bottom-3x mb-2 mt-2">
+    <div class="row">
+        <div class="col-lg-12">
+            <aside class="user-info-wrapper">
+                <div class="user-cover">
+                    <div class="info-label" title=""><i class="icon-medal"></i>290 pontos</div>
+                </div>
+                <div class="user-info">
+                    <div class="user-avatar">
+                        <a class="edit-avatar" href="#"></a><img src="images/utilizador.png" alt="User"></div>
+                    <div class="user-data">
+                        <h4><?php if(!empty($_SESSION['user_name'])) echo $username; else echo "Username não definido." ?></h4><span>Entrou em 06 de Maio de 2020</span>
+                    </div>
+                </div>
+            </aside>
+            <nav class="list-group">
+                <!--<a class="list-group-item with-badge" href="#"><i class="fa fa-th"></i>Orders<span class="badge badge-primary badge-pill">6</span></a>
+                <a class="list-group-item" href="#"><i class="fa fa-map"></i>Addresses</a>
+                <a class="list-group-item with-badge" href="#"><i class="fa fa-heart"></i>Wishlist<span class="badge badge-primary badge-pill">3</span></a>-->
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-    <link rel="stylesheet" href="style.css">
+                <a class="list-group-item" href="#"><i class="fa fa-user"></i> Perfil</a>
+                <a class="list-group-item with-badge bg-light " data-target="#myModal" data-toggle="modal" class="meusResultados" id="meusResultados" href="#myModal">
+                    <i class="fas fa-trophy"></i> Meus quizzes<span class="badge badge-warning badge-pill">4</span></a>
+            </nav>
+        </div>
+    </div>   
+</div>
 
-</head>
+    <div id="myModal" class="modal fade">
+          <form class="modal-content" action="" method="">
+            <div class="modal-header"></div>
+            <div class="container">
+              <h1>Meus resultados</h1>
+              <hr>
+              <?php
+                $sq = "SELECT id, user_id, category_id, score, num_questions FROM result";
+                    $result = $conn->query($sq);
+                    $user = [];
 
-<body>
-    <div><img class="w-100" src="images/underconstruction.jpg"></div> 
-    
-</body>
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            $user[] = $row;
+                        }
+                    } //else {
+                    //  echo "0 results";   
+                    //}
+                ?>
 
-</html>
+            <!--Botão Cancelar -->
+            <button value="Hover" type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Minha Conta</button>
+            </div>
+        </div>
+        </form>
+    </div>
+
+<?php
+    include "footer.html";
+?>
+
+
