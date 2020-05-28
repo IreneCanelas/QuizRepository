3a2b4494f8728489cb7ccb21533b3481861a7240
sararocha25 <sararaquel2621@gmail.com>
2020-05-28 23:21:18 +0100

    Correção de Bug no Botão "Voltar para trás"
    Fazer botão aparecer só quando há alguma pesquisa

--- quiz/index.php
+++ quiz/index.php
@@ -3,7 +3,16 @@
   include "header.php";
   //echo $_SESSION['user_id'];
   include "select_exam.php";
-  include "footer.html";
 
+
+  if ( isset($_GET['search'])) {
+    echo "<div class='text-center mb-3'> ";
+  echo "<form action='#pesquisa' name='search' method='GET'>";
+  echo " <button class='btn btn-outline-dark' type='submit'>Voltar ao início.</button> " ;
+ echo" </form>";
+echo "</div>";
+  }
+
+  include "footer.html";
  
 ?>
