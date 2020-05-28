93d3db21bc4545b781f774e68814cd4b76d19a68
Layla Reis <layla.sreis@gmail.com>
2020-05-28 10:54:18 +0100

    User name configured.

--- quiz/user.php
+++ quiz/user.php
@@ -2,6 +2,13 @@
     include "headerAfterLogin.php";
     include "connection.php";
     session_start();
+
+    //Base de Dados e Tabela client
+    $useridfinal = $_SESSION['user_id'];
+    $qry = "SELECT name FROM registos where id='$useridfinal'";
+    $result = mysqli_query($conn, $qry);
+    
+
 ?>
 
 <div class="container padding-bottom-3x mb-2 mt-2">
@@ -15,7 +22,12 @@
                     <div class="user-avatar">
                         <a class="edit-avatar" href="#"></a><img src="images/utilizador.png" alt="User"></div>
                     <div class="user-data">
-                        <h4><?php if(!empty($_SESSION['user_name'])) echo $username; else echo "Username não definido." ?></h4><span>Entrou em 06 de Maio de 2020</span>
+                        <h4><?php 
+                        if (mysqli_num_rows($result) > 0) {
+                            $row = mysqli_fetch_assoc($result);
+                            echo $row['name']; }
+                        
+                        else echo "Username não definido." ?></h4><span>Entrou em 06 de Maio de 2020</span>
                     </div>
                 </div>
             </aside>
@@ -23,8 +35,12 @@
                 <!--<a class="list-group-item with-badge" href="#"><i class="fa fa-th"></i>Orders<span class="badge badge-primary badge-pill">6</span></a>
                 <a class="list-group-item" href="#"><i class="fa fa-map"></i>Addresses</a>
                 <a class="list-group-item with-badge" href="#"><i class="fa fa-heart"></i>Wishlist<span class="badge badge-primary badge-pill">3</span></a>-->
-
+                
+                <!--Abrir perfil-->
                 <a class="list-group-item" href="#"><i class="fa fa-user"></i> Perfil</a>
+
+
+                <!--Abrir resultados do quiz-->
                 <a class="list-group-item with-badge bg-light " data-target="#myModal" data-toggle="modal" class="meusResultados" id="meusResultados" href="#myModal">
                     <i class="fas fa-trophy"></i> Meus quizzes<span class="badge badge-warning badge-pill">4</span></a>
             </nav>
