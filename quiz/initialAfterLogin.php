c6da6f5370098f607ecfccc869e74f812cc98e15
sararocha25 <sararaquel2621@gmail.com>
2020-06-02 10:54:00 +0100

    Bug - Search Button AfterLogin
    Correção do bug no botão de pesquisa depois de um login ter sido efetuado.

--- quiz/initialAfterLogin.php
+++ quiz/initialAfterLogin.php
@@ -3,37 +3,19 @@
 session_start();
 
   include "connection.php";
+  include "headerAfterLogin.php";
+  include "select_exam.php";
 
-  // Botao de Pesquisa na nav  
-  if(!empty($_GET['search'])) {
-    $search = $_GET['search'];
-    $sql = "SELECT id, name, photo_url FROM quizzes WHERE name='" . $search . "'";
-  } else {
-      $sql = "SELECT id, name, photo_url FROM quizzes";
+
+  if ( isset($_GET['search'])) {
+    echo "<div class='text-center mb-3'> ";
+      echo "<form action='#pesquisa' name='search' method='GET'>";
+        echo " <button class='btn btn-outline-dark' type='submit'>Voltar ao início.</button> " ;
+      echo" </form>";
+    echo "</div>";
   }
 
-  $result = $conn->query($sql);
-  $quizzes = [];
-
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-          $quizzes[] = $row;
-      }
-  } else {
-      echo "0 results";
-}
-
-include "headerAfterLogin.php"
-?>
-
-
-<!-- Ler user_id -->
-<!-- <p>Email: <?php  var_dump(@$_SESSION['user_email']) ?> </p> -->
-<!-- <p>ID: <?php  var_dump(@$_SESSION['user_id']) ?> </p> -->
-
-
-<?php
-  include "select_exam.php";
+ 
   include "footer.html";
-?>
 
+?>
